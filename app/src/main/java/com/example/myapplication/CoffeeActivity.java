06d055c79e2b639b75bbf64109d3cfcd8abbb2c8
Junsuk Oh <a811219@gmail.com>
2017-07-19 11:30:27 +0900

    Coffee 주문 앱  - 수량 조절

--- app/src/main/java/com/example/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/myapplication/CoffeeActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CoffeeActivity extends AppCompatActivity {
+
+    private TextView mQuantityTextView;
+    private int mQuantity = 1;
 
     // 메인
     @Override
@@ -16,15 +18,22 @@ public class CoffeeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coffee);
 
         // XML에 있는 View의 레퍼런스를 가져오는 방법
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text);
+        mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
 
-        quantityTextView.setText("10");
+//        mQuantityTextView.setText(String.valueOf(mQuantity));
+        mQuantityTextView.setText("" + mQuantity);
     }
 
     public void minusButtonClicked(View view) {
-        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+        mQuantity--;
+        if (mQuantity < 0) {
+            mQuantity = 0;
+        }
+        mQuantityTextView.setText("" + mQuantity);
     }
 
     public void plusButtonClicked(View view) {
+        mQuantity++;
+        mQuantityTextView.setText("" + mQuantity);
     }
 }
