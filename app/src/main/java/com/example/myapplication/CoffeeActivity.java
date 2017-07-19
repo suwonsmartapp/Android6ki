7d0fd55588c2220c9d0d39eaa306dc412fef2035
Junsuk Oh <a811219@gmail.com>
2017-07-19 11:47:17 +0900

    Coffee 주문 앱  - 수량에 따라 가격 변동되도록 수정

--- app/src/main/java/com/example/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/myapplication/CoffeeActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class CoffeeActivity extends AppCompatActivity {
 
     private TextView mQuantityTextView;
+    private TextView mPriceTextView;
     private int mQuantity = 1;
+
+    private DecimalFormat mFormat = new DecimalFormat("#,##0");
 
     // 메인
     @Override
@@ -19,21 +24,25 @@ public class CoffeeActivity extends AppCompatActivity {
 
         // XML에 있는 View의 레퍼런스를 가져오는 방법
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
+        mPriceTextView = (TextView) findViewById(R.id.price_text);
 
 //        mQuantityTextView.setText(String.valueOf(mQuantity));
         mQuantityTextView.setText("" + mQuantity);
+        mPriceTextView.setText(mFormat.format(mQuantity * 3000) + "원");
     }
 
     public void minusButtonClicked(View view) {
         mQuantity--;
-        if (mQuantity < 0) {
-            mQuantity = 0;
+        if (mQuantity < 1) {
+            mQuantity = 1;
         }
         mQuantityTextView.setText("" + mQuantity);
+        mPriceTextView.setText(mFormat.format(mQuantity * 3000) + "원");
     }
 
     public void plusButtonClicked(View view) {
         mQuantity++;
         mQuantityTextView.setText("" + mQuantity);
+        mPriceTextView.setText(mFormat.format(mQuantity * 3000) + "원");
     }
 }
