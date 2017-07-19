e8af9b3481c102a644f7789780769e330969da6b
Junsuk Oh <a811219@gmail.com>
2017-07-19 10:55:33 +0900

    Coffee 주문 앱  - XML에서 View의 레퍼런스 가져오는 방법, 버튼 조작, Toast 메시지

--- app/src/main/java/com/example/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/myapplication/CoffeeActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CoffeeActivity extends AppCompatActivity {
 
@@ -9,8 +12,19 @@ public class CoffeeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // 레이아웃 설정
         setContentView(R.layout.activity_coffee);
+
+        // XML에 있는 View의 레퍼런스를 가져오는 방법
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text);
+
+        quantityTextView.setText("10");
+    }
+
+    public void minusButtonClicked(View view) {
+        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+    }
+
+    public void plusButtonClicked(View view) {
     }
 }
