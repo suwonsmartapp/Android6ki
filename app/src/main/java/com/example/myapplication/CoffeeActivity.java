c444dd4a6c471e3aa88ff026072de9f0c124bae1
Junsuk Oh <a811219@gmail.com>
2017-07-20 11:31:58 +0900

    커피 앱 : EditText, 주문자 이름 받기

--- app/src/main/java/com/example/myapplication/CoffeeActivity.java
+++ app/src/main/java/com/example/myapplication/CoffeeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -17,6 +18,8 @@ public class CoffeeActivity extends AppCompatActivity {
     private TextView mQuantityTextView;
     private TextView mPriceTextView;
     private CheckBox mWippedCreamCheckBox;
+    private EditText mNameEditText;
+
     private int mQuantity = DEFAULT_QUANTITY;
 
     private DecimalFormat mFormat = new DecimalFormat("#,##0");
@@ -32,6 +35,7 @@ public class CoffeeActivity extends AppCompatActivity {
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
         mPriceTextView = (TextView) findViewById(R.id.price_text);
         mWippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check);
+        mNameEditText = (EditText) findViewById(R.id.name_edit);
 
 //        mQuantityTextView.setText(String.valueOf(mQuantity));
         display();
@@ -40,7 +44,9 @@ public class CoffeeActivity extends AppCompatActivity {
     private void display() {
         mQuantityTextView.setText("" + mQuantity);
 
-        String message = "휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
+        String message = "주문자 : " + mNameEditText.getText().toString();
+        message += "\n====================";
+        message += "\n휘핑 크림 추가 여부 : " + mWippedCreamCheckBox.isChecked();
         message += "\n갯수 : " + mQuantity;
         message += "\n가격 : " + mFormat.format(mQuantity * COFFEE_PRICE) + "원";
 
