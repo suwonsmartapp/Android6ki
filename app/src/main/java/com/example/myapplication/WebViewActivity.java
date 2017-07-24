7f971f64fbe1f01673256a224edc2b38f4ff026c
Junsuk Oh <a811219@gmail.com>
2017-07-24 11:46:16 +0900

    WebView :
    // 요거 해 줘야 된다
     mWebView.setWebViewClient(new WebViewClient());

---
+++ app/src/main/java/com/example/myapplication/WebViewActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private EditText mUrlEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mUrlEditText = (EditText) findViewById(R.id.url_edit);
+    }
+
+    public void showWebPage(View view) {
+        String url = mUrlEditText.getText().toString();
+        mWebView.loadUrl(url);
+
+        // 요거 해 줘야 된다
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+    public void goBack(View view) {
+        mWebView.goBack();
+    }
+
+    public void goForward(View view) {
+        mWebView.goForward();
+    }
+}
