84c07a438562065461d3143e804138c51c4e33cc
Junsuk Oh <a811219@gmail.com>
2017-07-27 11:39:47 +0900

    새 연락처 메뉴 추가

--- app/src/main/java/com/example/myapplication/adapterview/AdapterViewExamActivity.java
+++ app/src/main/java/com/example/myapplication/adapterview/AdapterViewExamActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication.adapterview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -79,5 +82,38 @@ public class AdapterViewExamActivity extends AppCompatActivity {
                 return true;       // 이벤트 소비를 하겠다. 더 이상 이벤트가 흘러가지 않는다
             }
         });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // 메뉴버튼이 처음 눌러졌을 때 실행되는 콜백메서드
+        // 메뉴버튼을 눌렀을 때 보여줄 menu 에 대해서 정의
+        getMenuInflater().inflate(R.menu.menu_address, menu);
+        Log.d("test", "onCreateOptionsMenu - 최초 메뉴키를 눌렀을 때 호출됨");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 메뉴의 항목을 선택(클릭)했을 때 호출되는 콜백메서드
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Log.d("test", "onOptionsItemSelected - 메뉴항목을 클릭했을 때 호출됨");
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_add:
+                Intent intent = new Intent(AdapterViewExamActivity.this,
+                        DetailAddressActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
