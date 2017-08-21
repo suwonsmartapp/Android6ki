fc7e56a5a6c504394a3c94e6b20c2391f6bd0df7
Junsuk Oh <a811219@gmail.com>
2017-08-21 10:23:07 +0900

    엔빵 앱 모듈로 생성, 레이아웃 구조 잡기

---
+++ npang/src/main/java/com/suwonsmartapp/warikang/MainActivity.java
@@ -0,0 +1,23 @@
+package com.suwonsmartapp.warikang;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFragment(new WarikanFragment());
+    }
+
+    private void initFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
+    }
+
+}
