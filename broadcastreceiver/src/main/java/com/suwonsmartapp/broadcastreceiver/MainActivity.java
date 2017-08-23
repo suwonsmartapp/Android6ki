a41d98db05681553dd37edda6bf8f9f5b6fc039b
Junsuk Oh <a811219@gmail.com>
2017-08-23 11:40:53 +0900

    로컬 브로드캐스트 리시버

--- broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MainActivity.java
+++ broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MainActivity.java
@@ -1,13 +1,37 @@
 package com.suwonsmartapp.broadcastreceiver;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyReceiver myReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myReceiver = new MyReceiver();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        registerReceiver(myReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(myReceiver);
     }
 }
