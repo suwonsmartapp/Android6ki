935a48ac75e6dfd1ed9512a253a0ab42e29d22bc
Junsuk Oh <a811219@gmail.com>
2017-08-24 16:35:37 +0900

    AdMob

--- npang/src/main/java/com/suwonsmartapp/warikang/MainActivity.java
+++ npang/src/main/java/com/suwonsmartapp/warikang/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             initFragment(new WarikanFragment());
         }
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("28A5DF817CB6C88082D4900F3C6D3C73")
+                .build();
+        mAdView.loadAd(adRequest);
     }
 
     private void initFragment(Fragment fragment) {
