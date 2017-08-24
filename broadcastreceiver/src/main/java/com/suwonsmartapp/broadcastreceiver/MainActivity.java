9fc081e1b0631828df98e522b90303aa1f04cc7a
Junsuk Oh <a811219@gmail.com>
2017-08-24 10:26:03 +0900

    로컬 브로드캐스트 리시버

--- broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MainActivity.java
+++ broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MainActivity.java
@@ -2,8 +2,9 @@ package com.suwonsmartapp.broadcastreceiver;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(MyReceiver.MY_ACTION);
 
         registerReceiver(myReceiver, filter);
     }
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         unregisterReceiver(myReceiver);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MyReceiver.MY_ACTION);
+        sendBroadcast(intent);
     }
 }
--- broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MyReceiver.java
+++ broadcastreceiver/src/main/java/com/suwonsmartapp/broadcastreceiver/MyReceiver.java
@@ -7,12 +7,16 @@ import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
 
+    public static final String MY_ACTION = "com.suwonsmartapp.broadcastreceiver.action.MY_ACTION";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())) {
             Toast.makeText(context, "전원 연결 됨", Toast.LENGTH_SHORT).show();
         } else if (Intent.ACTION_POWER_DISCONNECTED.equals(intent.getAction())) {
             Toast.makeText(context, "전원 뽑힘!!", Toast.LENGTH_SHORT).show();
+        } else if (MY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "나만의 액션이다", Toast.LENGTH_SHORT).show();
         }
     }
 }
