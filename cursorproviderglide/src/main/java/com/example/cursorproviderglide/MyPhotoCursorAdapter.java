65d2bace6e3ef044d989eb89b0e346360e4fe1d4
Junsuk Oh <a811219@gmail.com>
2017-08-25 11:43:49 +0900

    Glide 적용해서 사진이 잘 로드 됨

--- cursorproviderglide/src/main/java/com/example/cursorproviderglide/MyPhotoCursorAdapter.java
+++ cursorproviderglide/src/main/java/com/example/cursorproviderglide/MyPhotoCursorAdapter.java
@@ -2,13 +2,14 @@ package com.example.cursorproviderglide;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by junsuk on 2017. 8. 25..
@@ -21,17 +22,26 @@ public class MyPhotoCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.item_photo, parent, false);
+        ViewHolder viewHolder = new ViewHolder();
+        View view = LayoutInflater.from(context).inflate(R.layout.item_photo, parent, false);
+        viewHolder.imageView = view.findViewById(R.id.image_view);
+        view.setTag(viewHolder);
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        ImageView imageView = view.findViewById(R.id.image_view);
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
 
         // 그림이 데이터 베이스에서 실제로 몇번째 열에 있는지
         int index = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
         String data = cursor.getString(index);
 
-        imageView.setImageURI(Uri.parse(data));
+//        viewHolder.imageView.setImageURI(Uri.parse(data));
+        Glide.with(context).load(data).into(viewHolder.imageView);
+    }
+
+    private static class ViewHolder {
+        ImageView imageView;
     }
 }
