89a32d77257abe068d73b1b3c243fc53bd79f077
Junsuk Oh <a811219@gmail.com>
2017-08-28 10:00:51 +0900

    런타임 권한 체크 표준 https://developer.android.com/training/permissions/requesting.html

--- cursorproviderglide/src/main/java/com/example/cursorproviderglide/MainActivity.java
+++ cursorproviderglide/src/main/java/com/example/cursorproviderglide/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.cursorproviderglide;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 100;
 
     private GridView mGridView;
 
@@ -19,7 +28,39 @@ public class MainActivity extends AppCompatActivity {
 
         mGridView = (GridView) findViewById(R.id.grid_view);
 
-        // 권한 요청
+        // 권한 체크
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            // 권한 없으면 요청
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // 권한에 대한 설명
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("설정가서 권한 체크 할 수 있다");
+                builder.setNegativeButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+                builder.show();
+
+
+            } else {
+                // 권한 요청
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        } else {
+            loadPhoto();
+        }
+    }
+
+    private void loadPhoto() {
         Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 null,
                 null,
@@ -29,5 +70,31 @@ public class MainActivity extends AppCompatActivity {
         MyPhotoCursorAdapter adapter = new MyPhotoCursorAdapter(MainActivity.this, cursor);
 
         mGridView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // 권한 요청 응답 처리
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    loadPhoto();
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "권한이 없습니다", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
     }
 }
