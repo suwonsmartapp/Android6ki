e546fdaef63c31f53ff29c95e8389c3be040bb0b
Junsuk Oh <a811219@gmail.com>
2017-08-28 11:08:48 +0900

    Thread, Handler 사용법 2, 3

--- thread/src/main/java/com/example/thread/MainActivity.java
+++ thread/src/main/java/com/example/thread/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.thread;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -14,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView mPercentTextView;
 
     // Main Thread 로 전달하는 매개체
-    private Handler mHanlder;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mPercentTextView.setText((msg.arg1 + 1) + "%");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mPercentTextView = (TextView) findViewById(R.id.percent_text);
-
-        mHanlder = new Handler();
     }
 
     public void startDownload(View view) {
@@ -39,19 +44,24 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, "run: " + i);
                         mProgressBar.setProgress(i + 1);
 
-                        final int percent = i + 1;
-                        // UI 갱신
-                        mHanlder.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                mPercentTextView.setText(percent + "%");
-                            }
-                        });
+                        Message msg = Message.obtain();
+                        msg.arg1 = i;
+
+                        mHandler.sendMessage(msg);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
         }).start();
+    }
+
+    public void 오초_뒤에_토스트_띄우기(View view) {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "5초 되었음", Toast.LENGTH_SHORT).show();
+            }
+        }, 5000);
     }
 }
