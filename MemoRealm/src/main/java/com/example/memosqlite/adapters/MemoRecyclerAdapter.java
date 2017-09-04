d7f94ef8b56c19c617aa7cbf082b150878e1c054
Junsuk Oh <a811219@gmail.com>
2017-09-04 10:28:37 +0900

    Realm : 메모 앱 만들기 4, RealmRecyclerAdapter

--- MemoRealm/src/main/java/com/example/memosqlite/adapters/MemoRecyclerAdapter.java
+++ MemoRealm/src/main/java/com/example/memosqlite/adapters/MemoRecyclerAdapter.java
@@ -1,29 +1,40 @@
 package com.example.memosqlite.adapters;
 
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.memosqlite.models.Memo;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
 
 /**
  * Created by junsuk on 2017. 9. 4..
  */
 
-public class MemoRecyclerAdapter extends RecyclerView.Adapter<MemoRecyclerAdapter.ViewHolder> {
+public class MemoRecyclerAdapter extends RealmRecyclerViewAdapter<Memo, MemoRecyclerAdapter.ViewHolder> {
+
+    public MemoRecyclerAdapter(@Nullable OrderedRealmCollection<Memo> data) {
+        super(data, true);
+        setHasStableIds(true);
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_2, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
+        Memo memo = getItem(position);
+        holder.titleTextView.setText(memo.getTitle());
+        holder.memoTextView.setText(memo.getMemo());
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
--- MemoRealm/src/main/java/com/example/memosqlite/fragments/MemoListFragment.java
+++ MemoRealm/src/main/java/com/example/memosqlite/fragments/MemoListFragment.java
@@ -3,16 +3,21 @@ package com.example.memosqlite.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.memorealm.R;
+import com.example.memosqlite.adapters.MemoRecyclerAdapter;
+import com.example.memosqlite.models.Memo;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * Created by junsuk on 2017. 8. 30..
@@ -24,13 +29,23 @@ public class MemoListFragment extends Fragment {
     RecyclerView mRecyclerView;
     Unbinder unbinder;
 
+    private Realm mRealm;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_memo_list, container, false);
         unbinder = ButterKnife.bind(this, view);
+        mRealm = Realm.getDefaultInstance();
 
+        RealmResults<Memo> data = mRealm.where(Memo.class).findAll();
+        MemoRecyclerAdapter adapter = new MemoRecyclerAdapter(data);
 
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        mRecyclerView.setAdapter(adapter);
 
         return view;
     }
@@ -39,5 +54,6 @@ public class MemoListFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+        mRealm.close();
     }
 }
