13300c77c2d42a874877f61e97721ffd030b6eec
Junsuk Oh <a811219@gmail.com>
2017-09-05 11:16:50 +0900

    Realm : 메모 앱 만들기 8, 삭제, Undo

--- MemoRealm/src/main/java/com/example/memosqlite/adapters/MemoRecyclerAdapter.java
+++ MemoRealm/src/main/java/com/example/memosqlite/adapters/MemoRecyclerAdapter.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 import com.example.memorealm.R;
 import com.example.memosqlite.models.Memo;
 
+import java.util.Stack;
+
 import io.realm.OrderedRealmCollection;
+import io.realm.Realm;
 import io.realm.RealmRecyclerViewAdapter;
 
 /**
@@ -18,6 +21,30 @@ import io.realm.RealmRecyclerViewAdapter;
  */
 
 public class MemoRecyclerAdapter extends RealmRecyclerViewAdapter<Memo, MemoRecyclerAdapter.ViewHolder> {
+
+    private Stack<Memo> mUndoStack;
+
+    public void delete(Memo memo) {
+        Realm realm = Realm.getDefaultInstance();
+        mUndoStack.push(realm.copyFromRealm(memo));
+        realm.beginTransaction();
+
+        memo.deleteFromRealm();
+
+        realm.commitTransaction();
+        realm.close();
+    }
+
+    public void undo() {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+
+        Memo memo = mUndoStack.pop();
+        realm.insert(memo);
+
+        realm.commitTransaction();
+        realm.close();
+    }
 
     public interface OnItemClickListener {
         void onItemClicked(Memo memo);
@@ -31,6 +58,7 @@ public class MemoRecyclerAdapter extends RealmRecyclerViewAdapter<Memo, MemoRecy
 
     public MemoRecyclerAdapter(@Nullable OrderedRealmCollection<Memo> data) {
         super(data, true);
+        mUndoStack = new Stack<>();
     }
 
     @Override
@@ -54,11 +82,13 @@ public class MemoRecyclerAdapter extends RealmRecyclerViewAdapter<Memo, MemoRecy
         Memo memo = getItem(position);
         holder.titleTextView.setText(memo.getTitle());
         holder.memoTextView.setText(memo.getMemo());
+        holder.memo = memo;
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView titleTextView;
         TextView memoTextView;
+        public Memo memo;
 
         public ViewHolder(View itemView) {
             super(itemView);
--- MemoRealm/src/main/java/com/example/memosqlite/fragments/MemoListFragment.java
+++ MemoRealm/src/main/java/com/example/memosqlite/fragments/MemoListFragment.java
@@ -3,9 +3,11 @@ package com.example.memosqlite.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -56,6 +58,39 @@ public class MemoListFragment extends Fragment implements MemoRecyclerAdapter.On
 
         // Realm 에서 데이터 읽어오기
         RealmResults<Memo> data = mRealm.where(Memo.class).findAllSorted("id", Sort.DESCENDING);
+
+        // 터치
+        ItemTouchHelper touchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                return makeFlag(ItemTouchHelper.ACTION_STATE_DRAG, ItemTouchHelper.UP | ItemTouchHelper.DOWN) |
+                        makeFlag(ItemTouchHelper.ACTION_STATE_SWIPE, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                if (direction == ItemTouchHelper.LEFT) {
+                    Memo memo = ((MemoRecyclerAdapter.ViewHolder) viewHolder).memo;
+                    mAdapter.delete(memo);
+
+                    Snackbar.make(viewHolder.itemView, "삭제되었습니다", Snackbar.LENGTH_LONG)
+                            .setAction("취소", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    // 취소
+                                    mAdapter.undo();
+                                }
+                            }).show();
+                }
+            }
+        });
+        touchHelper.attachToRecyclerView(mRecyclerView);
+
 
         // 어댑터에 데이터 설정
         mAdapter = new MemoRecyclerAdapter(data);
