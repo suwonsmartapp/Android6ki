f2ea946d90bda4fce6bae305950512c37bb7b05d
Junsuk Oh <a811219@gmail.com>
2017-09-12 10:40:30 +0900

    현재 위치 정보 얻기

--- map/src/main/java/com/example/map/MapsActivity.java
+++ map/src/main/java/com/example/map/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.example.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    public static final int REQUEST_CODE_LOCATION_PERMISSION = 1000;
     private GoogleMap mMap;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,51 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLastLocation();
     }
 
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없으면 요청
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION);
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 17.0f));
+                            Toast.makeText(MapsActivity.this, location.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                // 승인 됨
+                getLastLocation();
+            } else {
+                // 안 됨
+                Toast.makeText(this, "승인 안 됨", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -45,6 +101,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng smartapp = new LatLng(37.274105, 127.02262100000007);
         mMap.addMarker(new MarkerOptions().position(smartapp).title("수원스마트앱개발학원"));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(smartapp, 17.0f));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(smartapp, 17.0f));
     }
 }
