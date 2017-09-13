8b0e1c7753056bc237099b6eb485fa8916e276cf
Junsuk Oh <a811219@gmail.com>
2017-09-13 09:59:38 +0900

    서비스는 메인스레드에서 동작한다

---
+++ service/src/main/java/com/example/service/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.service;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartService(View view) {
+        Intent service = new Intent(this, MyService.class);
+        startService(service);
+    }
+}
---
+++ service/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public static final String TAG = MyService.class.getSimpleName();
+
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                Log.d(TAG, "onStartCommand: " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
